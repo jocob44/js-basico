e68e517aed54325de9a29347b6bdc2f32dea3f8d
Joaquin E Islas <joa.islas@gmail.com>
2023-07-29 18:02:12 -0300

    including some samples

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+console.log(miNombre);
+var miNombre = "Pepe";
+
+
+var num = 40;
+num --;
+console.log(num);
+
+num -=2;
+
+console.log(num);
---
+++ valores.js
@@ -0,0 +1,16 @@
+40;
+
+"Diego de Granja";
+
+true;
+false;
+
+null;
+undefined;
+
+
+[1,2,4];
+
+{nombre: "Diego"}// objeto
+
+console.log(typeof 40);
